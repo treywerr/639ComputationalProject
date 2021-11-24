65759d9e69a7ebe32857355a5157008c9545a909
treywerr <treywerr@gmail.com>
2021-11-24 15:49:49 -0600

    Working implementation using symbolic math. Newton's method does not appear to work properly; diverges.

--- Newton.m
+++ Newton.m
@@ -1,8 +1,15 @@
 function [x,hist] = Newton(f,x0,maxSteps)
-    hist = zeros(maxSteps);
-    x = x0;
+    hist = zeros(2,maxSteps);
+    x = x0';
+    syms x1 x2;
+    hess = hessian(f,[x1,x2]);
+    grad = gradient(f,[x1,x2]);
     for k=1:maxSteps
-        s = linsolve( hessian(f(x(1),x(2)), gradient(f(x(1),x(2)))));
+        hist(1,k) = x(1);
+        hist(2,k) = x(2);
+        A = subs(hess, [x1,x2], [x(1),x(2)]);
+        B = subs(grad, [x1,x2], [x(1),x(2)]);
+        s = linsolve(double(A), double(B));
+        x = x + s;
     end
 end
-
--- TestFunctions.m
+++ TestFunctions.m
@@ -1,45 +1,38 @@
 % ---------- Plotting -------------
 % Rosenbrock
-x1 = linspace(-5,10);
-x2 = x1';
-z = f1(x1, x2);
+syms f1(x1,x2);
+f1(x1,x2) = 100*(x2 - x1.^2).^2 + (x1 - 1).^2;
 subplot(2,3,1);
-surf(x1,x2,z)
+fsurf(f1, [-5 10])
+[x,hist] = Newton(f1,[1.5 1],15);
+hold on; % Comment this out if you want to see the rosenbrock on its own.
+fcontour(f1, [-5 10])
+plot(hist(1,:),hist(2,:),'-.')
+plot(x(1),x(2),'o')
+hold off;
+
 % Camel
-x1 = linspace(-5,5);
-x2 = x1';
-z = f2(x1,x2);
+syms f2(x1,x2);
+f2(x1,x2) = 2*x1.^2 - 1.05*x1.^4 + x1.^6/6 + x1.*x2 + x2.^2;
 subplot(2,3,2);
-surf(x1,x2,z)
+fsurf(f2, [-5 5])
+
 % Michalewicz
+syms x1 x2 k;
+f3(x1,x2) = -sin(x1).*sin(x1.^2/pi).^(2*k) - sin(x2).*sin(2*x2.^2/pi).^(2*k);
+subplot(2,3,3);
+fsurf(subs(f3, k, 2), [0 pi])
+Newton(subs(f3,k,2),[1.5 1],2)
 
-
-%---------- Function Declarations -----------
-% Rosenbrock Function
-function [z] = f1(x1, x2)
-    z = 100*(x2 - x1.^2).^2 + (x1 - 1).^2;
-end
-
-% Three-hump Camel Function
-function [z] = f2(x1,x2)
-    z = 2*x1.^2 - 1.05*x1.^4 + x1.^6/6 + x1.*x2 + x2.^2;
-end
-
-% Michalewicz Function
-function [z] = f3(x)
-    k = 2; % find better way to deal with this parameter?
-    z = 0;
-    for i=1:2
-        z = z - sin(x(i))*sin(i*x(i)^2/pi)^(2*k);
-    end
-end
-
-% Ackley Function
-function [z] = f4(x)
-    z = -20*exp(-0.2*sqrt((x(1)^2 + x(2)^2)/2)) - exp((cos(2*pi*x(1)) + cos(2*pi*x(2)))/2) + 20 + exp(1);
-end
+% Ackley
+syms f4(x1,x2);
+f4(x1,x2) = -20*exp(-0.2*sqrt((x1.^2 + x2.^2)/2)) - exp((cos(2*pi*x1) + cos(2*pi*x2))/2) + 20 + exp(1);
+subplot(2,3,4);
+fsurf(f4,[-32.768 32.768])
 
 % Sixth Bukin Function
-function [z] = f5(x)
-    z = 100*sqrt(abs(x(2) - 0.01*x(1)^2)) + 0.01*abs(x(1)+10);
-end
+syms f5(x1,x2);
+f5(x1,x2) = 100*sqrt(abs(x2 - 0.01*x1.^2)) + 0.01*abs(x1+10);
+subplot(2,3,5);
+fsurf(f5,[-15 -5 -3 3])
+
