efe2b2c220f03ae15d4b64795fd30063a59fb148
chloekt <73850758+chloekt@users.noreply.github.com>
2021-11-27 14:26:18 -0600

    NelderMead outline

--- NelderMead.m
+++ NelderMead.m
@@ -1,17 +1,41 @@
-<<<<<<< HEAD
 function [x, hist] = NelderMead(f, x0, maxSteps)
 hist = zeros(2,maxSteps);
 x = x0';
 size = length(x);
 k=1;
-%alpha > 1
-while(tol || k<maxSteps)
+%set coefficients to standard values
+alpha = 1;
+gamma = 2;
+rho = 0.5;
+sigma = 0.5;
+while(norm < tol && k<maxSteps)
     %xc =centroid - not sure how to do that right now
-    xr = xc +  alpha(xc-x(size));
-    if(f(xr)>f(x(1)))
+    %reflection point
+    xr = xc +  alpha(xc-x(size)); 
+    if(f(xr)>=f(x(1))&& f(xr)<f(x(size-1)))
+        x(size)=xr;
+    %expansion
+    elseif(f(xr)<f(x(1)))
+        xe = xc + gamma*(xr-xc); %expanded point
+        if(f(xe)<f(xr))
+            x(size)=xe;
+        else
+            x(size)=xr;
+        end
+    else
+        %contraction
+        xcont = xc + rho*(x(size)-xc); %contracted point
+        if(f(xcont)<f(x(size)))
+            x(size)=xcont;
+        end
+        %else - shrink
+    end
 
-||||||| 9b9c7ac
-=======
-function x = NelderMead(f, x0, maxSteps)
+    
+    end
 
->>>>>>> 6089e2aea8104bc1324349b5f06ea49cd9142aba
+    
+    
+
+
+
