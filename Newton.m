93b68bd13e832a45dbbe550fccc6a9ce1c12bf01
Teddy Harbison <toharbison@gmail.com>
2021-11-28 22:34:19 -0600

    Made Newtons method work over n dimensions

--- Newton.m
+++ Newton.m
@@ -1,19 +1,20 @@
-function [x,hist] = Newton(f,x0,maxSteps)
-    hist = zeros(2,maxSteps); % History. Contains the point generated at each step.
-    x = x0';
-    syms x1 x2; % Declare the symbols so Newton can work with f symbolically
-    hess = hessian(f,[x1,x2]);
-    grad = gradient(f,[x1,x2]);
+function [min,hist] = Newton(f,initial,maxSteps)
+    hist = zeros(length(initial),maxSteps); % History. Contains the point generated at each step.
+    syms x [length(initial), 1]; % Declare the symbols so Newton can work with f symbolically
+    hess = hessian(f,x);
+    grad = gradient(f,x);
+    min = initial';
     for k=1:maxSteps
         % Store x_k
-        hist(1,k) = x(1);
-        hist(2,k) = x(2);
+        for i=1:length(initial)
+            hist(i,k) = min(i);
+        end
         % Substitute x_k into the hessian and gradient functions
-        A = subs(hess, [x1,x2], [x(1),x(2)]);
-        B = subs(grad, [x1,x2], [x(1),x(2)]);
+        A = subs(hess, x, min);
+        B = subs(grad, x, min);
         % Newton Step
         s = linsolve(double(A), -1*double(B)); % Must convert from symbolic to double. Destroys computer otherwise.
         % Update solution
-        x = x + s;
+        min = min + s;
     end
 end
