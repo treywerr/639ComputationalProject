dfb89d20b25986414694345cb219519dd29d23fe
Teddy Harbison <toharbison@gmail.com>
2021-11-30 16:45:09 -0600

    Implemented Conjugate Gradient, fixed line search to work correctly

--- lineSearch.m
+++ lineSearch.m
@@ -1,20 +1,23 @@
 function alpha = lineSearch(x, s, f)
 b=0.5;
+t=0.25;
 syms x1 x2;
 xc = x;
 alpha = 1;
-xt = xc-alpha*s;
+xt = xc+alpha*s;
 xt = double(xt);
 s = double(s);
-ft = f(xt(1), xt(2));
-fgoal = f(xc(1), xc(2))-(b*alpha*(s'*s));
+ft = f(xt(1),xt(2));
+grad(x1, x2) = gradient(f, [x1 x2]);
+g = double(grad(xc(1), xc(2)));
+fgoal = f(xc(1), xc(2))+(t*alpha*(g'*s));
 k=1;
-while(ft>=fgoal && k<10)
+while(ft>=fgoal && k<20)
     k=k+1;
     alpha = b*alpha;
-    xt = xc-(alpha*s);
-    ft = f(xt(1), xt(2));
-    fgoal = f(xc(1), xc(2))-(b*alpha*(s'*s));
+    xt = xc+(alpha*s);
+    ft = double(f(xt(1), xt(2)));
+    fgoal = double(f(xc(1), xc(2)))+(t*alpha*(g'*s));
 end
 
 end
