30bed78d5d0502d3b66c42fb56e96f95d03aafa7
chloekt <73850758+chloekt@users.noreply.github.com>
2021-12-04 17:12:28 -0600

    NM update

--- NelderMead.m
+++ NelderMead.m
@@ -1,7 +1,10 @@
 function [x, hist] = NelderMead(f, x0, maxSteps)
 hist = zeros(2,maxSteps);
 x = x0';
+x=double(x);
 size = length(x);
+n=size-1;
+sum = 0;
 k=1;
 %set coefficients to standard values
 alpha = 1;
@@ -9,33 +12,36 @@ gamma = 2;
 rho = 0.5;
 sigma = 0.5;
 while(norm < tol && k<maxSteps)
-    %xc =centroid - not sure how to do that right now
+    for j=1:n+1
+        sum = sum + x(j);
+    end
+    xc = sum/(n+1); % centroid
     %reflection point
-    xr = xc +  alpha(xc-x(size)); 
-    if(f(xr)>=f(x(1))&& f(xr)<f(x(size-1)))
-        x(size)=xr;
+    xr = xc + alpha(xc-x(n+1)); 
+    if(f(xr)>=f(x(1))&& f(xr)<f(x(n)))
+        x(n+1)=xr;
     %expansion
     elseif(f(xr)<f(x(1)))
         xe = xc + gamma*(xr-xc); %expanded point
         if(f(xe)<f(xr))
-            x(size)=xe;
+            x(n+1)=xe;
         else
-            x(size)=xr;
+            x(n+1)=xr;
         end
     else
         %contraction
-        xcont = xc + rho*(x(size)-xc); %contracted point
-        if(f(xcont)<f(x(size)))
-            x(size)=xcont;
-        end
+        xcont = xc + rho*(x(n+1)-xc); %contracted point
+        if(f(xcont)<f(x(n+1)))
+            x(n+1)=xcont;
         %else - shrink
+        else
+            for i=2:n+1
+                x(i) = x(1)-(sigma*(x(i)-x(1)));
+            end
+        end
     end
-
-    
-    end
+end
 
     
     
-
-
 
