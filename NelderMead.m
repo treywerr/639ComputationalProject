d53a1ff483510a2ba9a0e1e2e8a411e8b7092dad
chloekt <73850758+chloekt@users.noreply.github.com>
2021-12-06 12:05:59 -0600

    NM update?

--- NelderMead.m
+++ NelderMead.m
@@ -1,10 +1,11 @@
 function [x, hist] = NelderMead(f, x0, maxSteps)
-hist = zeros(2,maxSteps);
-x = x0';
+hist = zeros(length(x0),maxSteps)'; % History. Contains the point generated at each step.
+%syms x [length(initial), 1];
+x = x0';% need NM to work in N dimensions, f(x) needs to be f(x1, x2)
 x=double(x);
 size = length(x);
 n=size-1;
-sum = 0;
+sum = zeros(2,1);
 k=1;
 tol=0.001;
 %set coefficients to standard values
@@ -12,32 +13,33 @@ alpha = 1;
 gamma = 2;
 rho = 0.5;
 sigma = 0.5;
-while(abs(f(x(k))-(f(x(k+1)))) > tol && k<maxSteps)
+while(abs(f(x(1,k),x(2,k))-(f(x(1,k+1), x(2,k+1)))) > tol && k<maxSteps)
+    k=k+1;
     for j=1:n+1
         sum = sum + x(j);
     end
-    xc = sum/(n+1); % centroid
+    xc = sum/(n+1); % centroid - how do we do this for a vector?
     %reflection point
-    xr = xc + alpha(xc-x(n+1)); 
-    if(f(xr)>=f(x(1))&& f(xr)<f(x(n)))
-        x(n+1)=xr;
+    xr = xc + alpha(xc-x(:,n+1)); 
+    if(f(xr(1), xr(2))>=f(x(1,1), x(1,2))&& f(xr(1), xr(2))<f(x(1,n),x(2,n)))
+        x(:,n+1)=xr;
     %expansion
-    elseif(f(xr)<f(x(1)))
+    elseif(f(xr(1), xr(2))<f(x(1,1), x(2,1)))
         xe = xc + gamma*(xr-xc); %expanded point
-        if(f(xe)<f(xr))
-            x(n+1)=xe;
+        if(f(xe(1), xe(2))<f(xr(1), xr(2)))
+            x(:,n+1)=xe;
         else
-            x(n+1)=xr;
+            x(:,n+1)=xr;
         end
     else
         %contraction
-        xcont = xc + rho*(x(n+1)-xc); %contracted point
-        if(f(xcont)<f(x(n+1)))
-            x(n+1)=xcont;
+        xcont = xc + rho*(x(:,n+1)-xc); %contracted point
+        if(f(xcont(1), xcont(2))<f(x(1,n+1),x(2, n+1)))
+            x(:,n+1)=xcont;
         %else - shrink
         else
             for i=2:n+1
-                x(i) = x(1)-(sigma*(x(i)-x(1)));
+                x(:,i) = x(1)-(sigma*(x(:,i)-x(:,1)));
             end
         end
     end
